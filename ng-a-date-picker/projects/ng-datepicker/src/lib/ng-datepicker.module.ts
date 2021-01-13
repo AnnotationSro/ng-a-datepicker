8dc964b84140100986689ccca66f03f59d2b1c44
Martin Filo <martin.filo@annotation.sk>
2021-01-13 17:30:46 +0100

    fix build

--- ng-a-date-picker/projects/ng-datepicker/src/lib/ng-datepicker.module.ts
+++ ng-a-date-picker/projects/ng-datepicker/src/lib/ng-datepicker.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgDatepickerComponent } from './ng-datepicker/ng-datepicker.component';
-import { AnnotationDateDirective } from './directives/annotation-date.directive';
+import { AnnotationDateDirective } from './directives/date/annotation-date.directive';
 
 @NgModule({
   declarations: [NgDatepickerComponent, AnnotationDateDirective],
